6693be1f701da1a31df56f5824177c5fa2da05c8
Han <camryn202402855@hufs.ac.kr>
2024-05-17 22:11:47 +0900

    Update serializers.py

--- carpools/serializers.py
+++ carpools/serializers.py
@@ -4,4 +4,4 @@ from rest_framework import serializers
 class CarpoolSerializer(serializers.ModelSerializer):
     class Meta:
         model = Carpool
-        fields = ['post_num', 'title', 'type', 'client_gender','dept', 'dest', 'member', 'price', ,'carpool_date', 'created_at', 'end_carpool', 'CarpoolListView']
+        fields = ['post_num', 'title', 'type', 'client_gender','dept', 'dest', 'member', 'price', 'carpool_date', 'created_at', 'end_carpool', 'CarpoolListView']
