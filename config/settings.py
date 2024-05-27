778692e276bca03b3b5078e81d61744452372e54
sm4640 <s1m0611@naver.com>
2024-05-27 22:08:35 +0900

    스케줄러 제거

--- config/settings.py
+++ config/settings.py
@@ -57,7 +57,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'django_apscheduler',
+    # 'django_apscheduler',
     'accounts',
     'carpools',
 ]
