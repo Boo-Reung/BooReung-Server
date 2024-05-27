b11e9ea8d718176dc33d9309b653032aefd1ce30
sm4640 <s1m0611@naver.com>
2024-05-28 00:45:42 +0900

    cors 해결

--- config/settings.py
+++ config/settings.py
@@ -57,12 +57,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     # 'django_apscheduler',
     'accounts',
     'carpools',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware', 
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
